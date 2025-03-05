7c581dd0aeb9ab98ff115d16a91b957f3771b1ea
JonasVgt <git@jonasvoigt.de>
2025-03-05 18:12:05 +0100

    implement new graph visualization algorithm

--- src/visualization_controller/mod.rs
+++ src/visualization_controller/mod.rs
@@ -1,9 +1,9 @@
-use std::{cmp::Ordering, collections::HashMap};
+use std::{cmp::min, collections::HashMap};
 
-use egui::{pos2, vec2, Pos2};
+use egui::Pos2;
 use petgraph::{
-    algo::{has_path_connecting, tarjan_scc},
-    graph::node_index,
+    algo::condensation,
+    graph::{node_index, NodeIndex},
     Directed, Graph,
 };
 
@@ -26,40 +26,115 @@ impl<'a> VisualizationController {
 
         data.relations.iter().for_each(|relation| {
             graph.add_edge(
-                node_index(node_indices.get(&relation.calculate_from_hash()).unwrap().clone()),
-                node_index(node_indices.get(&relation.calculate_to_hash()).unwrap().clone()),
+                node_index(
+                    node_indices
+                        .get(&relation.calculate_from_hash())
+                        .unwrap()
+                        .clone(),
+                ),
+                node_index(
+                    node_indices
+                        .get(&relation.calculate_to_hash())
+                        .unwrap()
+                        .clone(),
+                ),
                 relation.relation_type,
             );
         });
 
         Self { graph }
     }
-    pub fn arrange(&self) -> HashMap<u64, Pos2> {
-        let mut levels = tarjan_scc(&self.graph);
-        levels.sort_unstable_by(|nodes1, nodes2| {
-            let node1 = nodes1.get(0).unwrap();
-            let node2 = nodes2.get(0).unwrap();
-            if has_path_connecting(&self.graph, node1.clone(), node2.clone(), None) {
-                Ordering::Greater
-            } else if has_path_connecting(&self.graph, node1.clone(), node2.clone(), None) {
-                Ordering::Less
-            } else {
-                Ordering::Equal
+    pub fn arrange(self) -> HashMap<u64, Pos2> {
+        // An Directed Acyclic Graph containting the complexity classes. Equal classes are stored in a single node
+        let condensated_graph = condensation(self.graph, true);
+
+        let mut level_map: HashMap<NodeIndex, i32> = HashMap::new();
+        let mut not_done = Vec::new();
+
+        let leafs: Vec<NodeIndex> = condensated_graph
+            .node_indices()
+            .filter(|node| {
+                condensated_graph
+                    .neighbors_directed(node.clone(), petgraph::Direction::Outgoing)
+                    .next()
+                    .is_none()
+            })
+            .collect();
+
+        for leaf in leafs {
+            level_map.insert(leaf, 0);
+            not_done.push(leaf);
+        }
+
+        while let Some(node) = not_done.pop() {
+            let id = level_map.get(&node).unwrap() + 1;
+            for neighbor in
+                condensated_graph.neighbors_directed(node, petgraph::Direction::Incoming)
+            {
+                if level_map.contains_key(&neighbor) {
+                    level_map.insert(neighbor, min(level_map.get(&neighbor).unwrap().clone(), id));
+                } else {
+                    level_map.insert(neighbor, id);
+                    not_done.push(neighbor);
+                }
             }
-        });
+        }
+
+        let mut levels: Vec<Vec<NodeIndex>> =
+            level_map
+                .into_iter()
+                .fold(vec![], |mut accu, (node, level)| {
+                    if accu.len() < level as usize + 1 {
+                        accu.resize(level as usize + 1, vec![]);
+                    }
+                    accu[level as usize].push(node);
+                    accu
+                });
+
+        let heur = |node: NodeIndex, parent_level: &Vec<NodeIndex>| {
+            let mut sum = 0;
+            let mut num = 0;
+            let neighbors: Vec<NodeIndex> = condensated_graph
+                .neighbors_directed(node, petgraph::Direction::Outgoing)
+                .collect();
+            let mut i = 0;
+            for parent in parent_level {
+                if neighbors.contains(&parent) {
+                    sum += i;
+                    num += 1;
+                }
+                i += 1;
+            }
+            return (10000.0 * (sum as f32 / num as f32)) as i32;
+        };
+
+        for i in 1..levels.len() {
+            let (done, unsorted) = levels.split_at_mut(i);
+            println!(
+                "{:?}",
+                unsorted[0]
+                    .iter()
+                    .map(|node| (node.clone(), heur(node.clone(), done.last().unwrap())))
+                    .collect::<Vec<_>>()
+            );
+            unsorted[0].sort_by_key(|node| heur(node.clone(), done.last().unwrap()));
+        }
 
         let mut map: HashMap<u64, Pos2> = HashMap::new();
 
-        let mut pos = pos2(0.0, 0.0);
+        let mut x = 0;
         for level in levels {
-            pos = pos2(pos.x + 100.0, 0.0);
+            let mut y = 0;
 
-            for node in level {
-                let id = self.graph.node_weight(node).unwrap();
-
-                map.insert(id.clone(), pos);
-                pos += vec2(0.0, 100.0);
+            for node in level
+                .iter()
+                .flat_map(|node| condensated_graph.node_weight(node.clone()).unwrap())
+            {
+                let pos = Pos2::new(x as f32 * 100.0, y as f32 * 100.0);
+                map.insert(node.clone(), pos);
+                y += 1;
             }
+            x += 1;
         }
         return map;
     }
