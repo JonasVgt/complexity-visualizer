d166703eb8c5728eead4f6737f5dda72bf9d6545
JonasVgt <git@jonasvoigt.de>
2025-03-07 13:31:51 +0100

    restyle sidepanel heading

--- src/sidepanel/mod.rs
+++ src/sidepanel/mod.rs
@@ -1,16 +1,14 @@
 use crate::{
-    database::complexity_class::ComplexityClass, rich_label, utils::text_parser::RichTextParser,
+    database::complexity_class::ComplexityClass, rich_label, rich_label_heading, utils::text_parser::RichTextParser
 };
 
 pub fn ui_sidepanel(ui: &mut egui::Ui, class: Option<&ComplexityClass>) {
     if let Some(c) = class {
+        
+        ui.add(rich_label_heading!(c.names.first().unwrap().clone()));
         ui.horizontal(|ui| {
-            for name in &c.names {
-                let label = RichTextParser::new()
-                    .parse(name.clone())
-                    .text_syle(egui::TextStyle::Heading)
-                    .to_label();
-                ui.add(label);
+            for name in &c.names[1..] {
+                ui.add(rich_label!(name.clone()));
                 ui.add_space(10.0);
             }
         });
