22eeb217f25cc704c53f5b39b60be39ba3d3f6ed
JonasVgt <git@jonasvoigt.de>
2025-03-07 13:24:13 +0100

    add rich text parsing to heading of sidepanel

--- src/sidepanel/mod.rs
+++ src/sidepanel/mod.rs
@@ -4,8 +4,16 @@ use crate::{
 
 pub fn ui_sidepanel(ui: &mut egui::Ui, class: Option<&ComplexityClass>) {
     if let Some(c) = class {
-        let heading = c.names.join(" | ");
-        ui.heading(&heading);
+        ui.horizontal(|ui| {
+            for name in &c.names {
+                let label = RichTextParser::new()
+                    .parse(name.clone())
+                    .text_syle(egui::TextStyle::Heading)
+                    .to_label();
+                ui.add(label);
+                ui.add_space(10.0);
+            }
+        });
         ui.separator();
         ui.add(rich_label!(c.description.clone()));
         ui.separator();
