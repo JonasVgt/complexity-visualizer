ff0ae168c6b08b2ea290ed6abf81a8c9c5fbb1ab
JonasVgt <git@jonasvoigt.de>
2025-04-03 10:51:09 +0200

    build.rs

--- build.rs
+++ build.rs
@@ -1,8 +1,16 @@
 use std::process::Command;
 
 fn main() {
-
+    println!("Running build.rs script...");
     // Generate data as msgpack
     println!("cargo::rerun-if-changed=data/");
-    Command::new("python3").args(&["data/gen_msgpack.py"]).status().unwrap();
+    let status = Command::new("python3").args(&["data/gen_msgpack.py"]).status().unwrap();
+
+    if status.success() {
+        println!("Python script executed successfully.");
+    } else {
+        eprintln!("Python script failed with status: {}", status);
+    }
+    
+    println!("Ran build.rs script...");
 }
